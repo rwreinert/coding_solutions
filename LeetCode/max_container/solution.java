44c46e7a17af511d8df8ea4d771fe20a4868dd1a
Robert Reinert <rreinert2014@gmail.com>
2019-03-10 21:03:43 -0400

    Adds max container java brute force solution.

---
+++ LeetCode/max_container/solution.java
@@ -0,0 +1,31 @@
+/*
+Given n non-negative integers a1, a2, ..., an , where each represents a point at coordinate (i, ai).
+n vertical lines are drawn such that the two endpoints of line i is at (i, ai) and (i, 0). Find two
+lines, which together with x-axis forms a container, such that the container contains the most water.
+
+Note: You may not slant the container and n is at least 2.
+ */
+
+
+class Solution {
+    public int maxArea(int[] height) {
+        Integer maxArea = Integer.MIN_VALUE;
+        Integer tempArea;
+        if(height.length < 2)
+            return -1;
+        // Iterative brute force method:
+
+        // For every elt, find its max area potentail
+        for(int i =0; i< height.length; i++) {
+
+            for(int j = i; j<height.length; j++) {
+
+                tempArea = Math.min(height[i], height[j]) * Math.abs(i-j);
+
+                if(tempArea > maxArea)
+                    maxArea = tempArea;
+            }
+        }
+        return maxArea;
+    }
+}
