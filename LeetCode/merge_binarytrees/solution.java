229b45bc262cf0c7e9703d7a6c65859170d86062
Robert Reinert <rreinert2014@gmail.com>
2019-04-10 13:14:29 -0400

    Adds new solution.

---
+++ LeetCode/merge_binarytrees/solution.java
@@ -0,0 +1,52 @@
+/**
+ * Problem:
+ * Given two binary trees and imagine that when you put one of them to cover the other, some nodes of the two trees are overlapped while the others are not.
+ * You need to merge them into a new binary tree. The merge rule is that if two nodes overlap, then sum node values up as the new value of the merged node. Otherwise, the NOT null node will be used as the node of new tree.
+ *
+ * Results:
+ * Runtime: 1 ms, faster than 85.94% of Java online submissions for Merge Two Binary Trees.
+ * Memory Usage: 41 MB, less than 83.46% of Java online submissions for Merge Two Binary Trees.
+ */
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+
+        int sum = 0;
+        TreeNode newNode;
+
+        if(t1 == null && t2 == null) {
+            return null;
+        } else {
+            if(t1 != null && t2 == null) {
+                sum = t1.val;
+                newNode = new TreeNode(sum);
+                newNode.left = mergeTrees(t1.left, null);
+                newNode.right = mergeTrees(t1.right, null);
+            } else if(t1 == null && t2 != null) {
+                sum = t2.val;
+                newNode = new TreeNode(sum);
+                newNode.left = mergeTrees(null, t2.left);
+                newNode.right = mergeTrees(null, t2.right);
+            } else {
+                sum = t1.val + t2.val;
+                newNode = new TreeNode(sum);
+                newNode.left = mergeTrees(t1.left, t2.left);
+                newNode.right = mergeTrees(t1.right, t2.right);
+            }
+
+
+            return newNode;
+        }
+
+    }
+}
